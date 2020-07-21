20d453df604e172ab9b93acdb04b04b6738497b2
William Brannock <42524423+wbrannock@users.noreply.github.com>
2020-07-20 22:14:41 -0400

    Update casespercap.R

--- scripts/casespercap.R
+++ scripts/casespercap.R
@@ -4,8 +4,8 @@ library(lubridate)
 
 #Change date
 date <- "2020-07-20"
-cases_data <- read_csv("cases3.csv")
-income_data <- read_csv("incomekey.csv")
+cases_data <- read_csv("cases.csv")
+income_data <- read_csv("income.csv")
 cases_data$`Report Date` <- mdy(cases_data$`Report Date`)
 
 #Sort, Filter and Organize Data
@@ -16,5 +16,5 @@ final <- total[order(-total$percap),]
 top5 <- head(final, 5)
 
 #Generate plot
-barplot(top5$percap, main="VIrginia COVID Hotspots", horiz=TRUE,
+barplot(top5$percap, main="Virginia COVID Hotspots", horiz=TRUE,
         names.arg=c(top5$Locality), xlab="Cases per Capita (higher is worse)")
